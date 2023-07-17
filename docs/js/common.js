e11ddb37c20d24bcafd1e024cdf784de6ef67513
Alireza <habadyalireza@gmail.com>
2023-07-17 23:32:18 +0330

    Tools - Gear generator

---
+++ docs/js/common.js
@@ -0,0 +1,112 @@
+/*
+ *  Adds a new Gear card.
+ */
+
+$('#add-form').on('click', function (e) {
+  e.preventDefault()
+
+  // Get the last child element and clone it
+  let $lastCard = $("div[id^='card_']:last")
+  let newID = parseInt($lastCard.prop('id').match(/\d+/g), 10) + 1
+  let $cardClone = $lastCard.clone().prop('id', 'card_' + newID)
+  let $cardCloneID = '#' + $cardClone.attr('id')
+
+  // Append the clone to the form
+  $('#main-form').append($cardClone)
+
+  // Clear the input fields of the clone and change the ID
+  clearForm($cardCloneID)
+  setGearID($cardCloneID, 'loadout')
+})
+
+/*
+ *  Removes the selected card.
+ */
+
+$('.main-form-container').on('click', '.card-remove', function (e) {
+  e.preventDefault()
+
+  $(this).closest('.card').remove()
+
+  // Re-assign ID's
+  refreshGearIDs()
+})
+
+/*
+ *  Refreshes the gear IDs.
+ */
+function refreshGearIDs() {
+  let cards = $('.main-form-container').find("div[id^='card_']")
+
+  Array.from(cards).forEach(function (e, i) {
+    setCardID(e, i)
+  })
+}
+
+/*
+ *  Event fired when the user submits the form.
+ */
+$('#main-form').submit((e) => {
+  e.preventDefault()
+
+  generateOutput()
+})
+
+/*
+ *  Refreshes the ID of a card.
+ */
+function setCardID(oldID, index) {
+  let newID = 'card_' + index
+
+  $(oldID).prop('id', newID)
+  const currentRoute = window.location.pathname
+  let label = currentRoute === '/gear_creator.html' ? 'Loadout' : 'Intel'
+  setGearID('#' + newID, label)
+}
+
+/*
+ *  Changes the card header according to the ID of the gear card.
+ */
+function setGearID(cardID, label) {
+  let gearTitle = label + ' #' + cardID.split('_')[1]
+
+  $(cardID).find('h3').html(gearTitle)
+}
+
+/*
+ *  Clears the form after cloning it.
+ */
+function clearForm(cardID) {
+  let formElements = $(cardID).find('.user-input')
+
+  Array.from(formElements).forEach((e) => {
+    $(e).attr('type') === 'checkbox'
+      ? $(e).prop('checked', false)
+      : $(e).val('')
+  })
+}
+
+/*
+ *  Converts the line breaks to "<br/>".
+ */
+function convertLineBreaks(str) {
+  return str.split('\n').join('\n<br/>')
+}
+
+/*
+ *  Copy to clipboard event fired when the user clicks the button.
+ */
+$('#copy-button').click((e) => {
+  let text = $('#output')
+
+  text.select()
+  document.execCommand('copy')
+  document.getSelection().removeAllRanges()
+
+  // Button text
+  $('#panel').slideDown('slow')
+
+  setTimeout(function () {
+    $('#panel').slideUp('slow')
+  }, 3000)
+})
---
+++ docs/js/gear_creator.js
@@ -0,0 +1,104 @@
+/*
+ *  Gear creator made for MalFramework script library for Arma 3.
+ *
+ *  GitHub: https://github.com/Malbryn/MalFramework
+ */
+
+// Output array
+let gearArrays = []
+
+/*
+ *  Generates the gear template.
+ */
+function generateOutput() {
+  let cards = $('.main-form-container').find("div[id^='card_']")
+
+  Array.from(cards).forEach((e) => {
+    let formElements = $(e).find('.user-input')
+    let gearArray = []
+
+    Array.from(formElements).forEach((e) => {
+      gearArray.push(convertLineBreaks($(e).val()))
+    })
+
+    gearArrays.push(gearArray)
+  })
+
+  let template = `/* This gear file was generated with the gear Creator tool
+    Create a loadout in the ACE Arsenal, click 'Export' and then paste the exported array.
+
+    Use selectRandom to randomise stuff.
+    Example:
+
+    case "SL" : {
+        _randomStuff = selectRandom ["stuff1_classname", "stuff2_classname"];
+
+        _gear = [
+            ...,
+            _randomStuff,
+            ...
+        ];
+    };
+ */
+
+// CTRG 2035
+
+  ${formatGearArrays(gearArrays)}`
+
+  // Display output
+  $('#output').val(template)
+  $('#output-field').css('display', 'flex')
+
+  // Scroll down
+  $('#output-field')[0].scrollIntoView({
+    behavior: 'smooth',
+    block: 'start',
+  })
+  // Reset output
+  gearArrays = []
+}
+
+/**
+ *  Format the output array.
+ */
+function formatGearArrays(gearArrays) {
+  let templateString = ''
+  const comments = [
+    '// Primary Weapon',
+    '// Launcher',
+    '// Secondary Weapon',
+    '// Uniform',
+    '// Vest',
+    '// Backpack',
+    '// Helmet',
+    '// Facewear',
+    '// Binos',
+    '// Linked Items',
+  ]
+
+  for (let i = 0; i < gearArrays.length; i++) {
+    let arsenalExport = gearArrays[i][1]
+    let role = gearArrays[i][0]
+
+    const replacedStr = arsenalExport.replace(/&quot;/g, '"') // Replace &quot; with " because JSON.parse() doesn't like it.
+    try {
+      const arr = JSON.parse(replacedStr)
+      templateString += `case "${role[i][0]}" : {\n\t_gear = [\n`
+      for (let j = 0; j < arr.length; j++) {
+        let loadout = arr[j]
+        for (let k = 0; k < loadout.length; k++) {
+          //loop through the 9 items
+          let item = loadout[k]
+          templateString += '\t\t' + comments[k] + '\n'
+          templateString += '\t\t' + JSON.stringify(item) + ',\n'
+        }
+        templateString = templateString.slice(0, -1)
+      }
+      templateString += `\n\t];\n};\n\n\n`
+    } catch {
+      alert('there was an error read the output')
+      return `Error: Invalid array in loadout ${i}. Make sure to copy the exported array from ACE Arsenal. `
+    }
+  }
+  return templateString
+}
