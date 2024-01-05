a6242282568e4cc8ec628bcb258bdaa3ea5652d0
Alireza <habadyalireza@gmail.com>
2024-01-05 16:14:13 +0330

    Gear creator - remove unnecessary element

--- docs/js/gear_creator.js
+++ docs/js/gear_creator.js
@@ -11,59 +11,58 @@ let gearArrays = []
  *  Generates the gear template.
  */
 function generateOutput() {
-  let cards = $('.main-form-container').find("div[id^='card_']")
+    let cards = $('.main-form-container').find("div[id^='card_']")
 
-  Array.from(cards).forEach((e) => {
-    let formElements = $(e).find('.user-input')
-    let gearArray = []
+    Array.from(cards).forEach((e) => {
+        let formElements = $(e).find('.user-input')
+        let gearArray = []
 
-    Array.from(formElements).forEach((e) => {
-      gearArray.push(convertLineBreaks($(e).val()))
+        // Log the ID of each form element
+        Array.from(formElements).forEach((e) => {
+            console.log(e.id) // Add this line
+            gearArray.push(convertLineBreaks($(e).val()))
+        })
+
+        gearArrays.push(gearArray)
     })
 
-    gearArrays.push(gearArray)
-  })
+    let template = `/* This gear file was generated with the gear Creator tool
+Create a loadout in the ACE Arsenal, click 'Export' and then paste the exported array.
 
-  let template = `/* This gear file was generated with the gear Creator tool
-    Create a loadout in the ACE Arsenal, click 'Export' and then paste the exported array.
+Use selectRandom to randomise stuff.
+Example:
 
-    Use selectRandom to randomise stuff.
-    Example:
+case "SL" : {
+_randomStuff = selectRandom ["stuff1_classname", "stuff2_classname"];
 
-    case "SL" : {
-        _randomStuff = selectRandom ["stuff1_classname", "stuff2_classname"];
+_gear = [
+...,
+_randomStuff,
+...
+];
+};
+*/
 
-        _gear = [
-            ...,
-            _randomStuff,
-            ...
-        ];
-    };
- */
+${formatGearArrays(gearArrays)}`
 
-// CTRG 2035
+    // Display output
+    $('#output').val(template)
+    $('#output-field').css('display', 'flex')
 
-  ${formatGearArrays(gearArrays)}`
-
-  // Display output
-  $('#output').val(template)
-  $('#output-field').css('display', 'flex')
-
-  // Scroll down
-  $('#output-field')[0].scrollIntoView({
-    behavior: 'smooth',
-    block: 'start',
-  })
-  // Reset output
-  gearArrays = []
+    // Scroll down
+    $('#output-field')[0].scrollIntoView({
+        behavior: 'smooth',
+        block: 'start',
+    })
+    // Reset output
+    gearArrays = []
 }
 
 /**
  *  Format the output array.
  */
-function formatGearArrays(gearArrays) {
-  let templateString = ''
-  const comments = [
+
+const comments = [
     '// Primary Weapon',
     '// Launcher',
     '// Secondary Weapon',
@@ -74,32 +73,32 @@ function formatGearArrays(gearArrays) {
     '// Facewear',
     '// Binos',
     '// Linked Items',
-  ]
+]
 
-  for (let i = 0; i < gearArrays.length; i++) {
-    let arsenalExport = gearArrays[i][1]
-    let role = gearArrays[i][0]
+function formatGearArrays(gearArrays) {
+    let templateString = ''
 
-    const replacedStr = arsenalExport.replace(/&amp;quot;/g, '"') // Replace &amp;quot; with " because JSON.parse() doesn't like it.
-    const arr = JSON.parse(replacedStr)
+    for (let i = 0; i < gearArrays.length; i++) {
+        let arsenalExport = gearArrays[i][1]
+        let role = gearArrays[i][0]
 
-    templateString += `case "${role}" : {\n\t_gear = [\n`
+        const replacedStr = arsenalExport.replace(/&quot;/g, '"') // Replace &quot; with " because JSON.parse() doesn't like it.
+        const arr = JSON.parse(replacedStr)
 
-    for (let j = 0; j < arr.length; j++) {
-      let loadout = arr[j]
+        templateString += `case "${role}" : {\n\t_gear = [\n`
 
-      // loop through the 9 items.
-      for (let k = 0; k < loadout.length; k++) {
-        let item = loadout[k]
-        templateString += '\t\t' + comments[k] + '\n'
-        templateString += '\t\t' + JSON.stringify(item) + ',\n'
-      }
+        let loadout = arr[0]
 
-      templateString = templateString.slice(0, -1)
+        for (let k = 0; k < Math.min(loadout.length, 10); k++) {
+            let item = loadout[k]
+            templateString += '\t\t' + comments[k] + '\n'
+            templateString += '\t\t' + JSON.stringify(item) + ',\n'
+        }
+
+        templateString = templateString.slice(0, -1)
+
+        templateString += `\n\t];\n};\n\n\n`
     }
 
-    templateString += `\n\t];\n};\n\n\n`
-  }
-
-  return templateString
+    return templateString
 }
--- docs/js/intel_creator.js
+++ docs/js/intel_creator.js
@@ -11,68 +11,68 @@ let output = []
  *  Generates the intel template.
  */
 function generateOutput() {
-  let cards = $('.main-form-container').find("div[id^='card_']")
+    let cards = $('.main-form-container').find("div[id^='card_']")
 
-  Array.from(cards).forEach((e) => {
-    let formElements = $(e).find('.user-input')
-    let intelArray = []
+    Array.from(cards).forEach((e) => {
+        let formElements = $(e).find('.user-input')
+        let intelArray = []
 
-    Array.from(formElements).forEach((e) => {
-      intelArray.push(
-        $(e).attr('type') === 'checkbox'
-          ? $(e).prop('checked')
-          : convertLineBreaks($(e).val())
-      )
+        Array.from(formElements).forEach((e) => {
+            intelArray.push(
+                $(e).attr('type') === 'checkbox'
+                    ? $(e).prop('checked')
+                    : convertLineBreaks($(e).val())
+            )
+        })
+
+        // Convert the duration to integer
+        intelArray[2] = parseInt(intelArray[2])
+
+        output.push(intelArray)
     })
 
-    // Convert the duration to integer
-    intelArray[2] = parseInt(intelArray[2])
-
-    output.push(intelArray)
-  })
-
-  let template = `// This intel file was generated with the Intel Creator tool
+    let template = `// This intel file was generated with the Intel Creator tool
 
 EGVAR(intel,intelList) = [${formatOutput(output)}];
     `
 
-  // Display output
-  $('#output').val(template)
-  $('#output-field').css('display', 'flex')
+    // Display output
+    $('#output').val(template)
+    $('#output-field').css('display', 'flex')
 
-  // Scroll down
-  $('#output-field')[0].scrollIntoView({
-    behavior: 'smooth',
-    block: 'start',
-  })
+    // Scroll down
+    $('#output-field')[0].scrollIntoView({
+        behavior: 'smooth',
+        block: 'start',
+    })
 
-  // Reset output
-  output = []
+    // Reset output
+    output = []
 }
 
 /*
  *  Format the output array.
  */
 function formatOutput(output) {
-  let templateString = ''
+    let templateString = ''
 
-  output.forEach((e) => {
-    templateString +=
-      '["' +
-      e[0] +
-      '", "' +
-      e[1] +
-      '", ' +
-      e[2] +
-      ', ' +
-      e[3] +
-      ', ' +
-      e[4] +
-      '], '
-  })
+    output.forEach((e) => {
+        templateString +=
+            '["' +
+            e[0] +
+            '", "' +
+            e[1] +
+            '", ' +
+            e[2] +
+            ', ' +
+            e[3] +
+            ', ' +
+            e[4] +
+            '], '
+    })
 
-  // Delete the last comma
-  templateString = templateString.slice(0, -2)
+    // Delete the last comma
+    templateString = templateString.slice(0, -2)
 
-  return templateString
+    return templateString
 }
